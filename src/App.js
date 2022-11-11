cc67600489d8771364865de97d95376852f754d9
Tetz <xenosign@naver.com>
2022-11-11 22:00:46 +0900

    작업 중!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,79 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 class App {
-  play() {}
+  constructor() {
+    this.money = 0;
+    this.counts = 0;
+    this.lottoNumbers = [];
+    this.bonus = 0;
+    this.userLottoNumbers = [];
+  }
+
+  getUserInput() {
+    MissionUtils.Console.readLine("로또 구입 금액을 입력하세요 : ", (userInput) => {
+      const inputNumber = parseInt(userInput);
+
+      if (inputNumber % 1000 !== 0) {
+        throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요");
+      }
+
+      if (inputNumber === 0) {
+        throw new Error("[ERROR] 구입 금액은 1,000원 이상으로 입력해 주세요");
+      }
+      this.money = userInput;
+      this.counts = this.money / 1000;
+      this.getLottoInput();
+    });
+  }
+
+  getLottoInput() {
+    MissionUtils.Console.readLine("로또 당첨 번호를 입력하세요(',' 쉼표로 구분) : ", (userInput) => {
+      const userLottoArr = userInput.split(",").map(Number);
+
+      if (new Set(userLottoArr).size !== 6) {
+        throw new Error("[ERROR] 중복 된 숫자를 입력 하셨거나, 숫자 6개를 입력 하시지 않으셨습니다")
+      }
+
+      if (userLottoArr.filter(el => el >= 1 && el <= 45).length !== 6) {
+        throw new Error("[ERROR] 1 ~ 45 사이의 숫자를 입력하세요");
+      }
+
+      this.userLottoNumbers = userLottoArr;
+
+      this.getBonusInput();
+    });
+  }
+
+  getBonusInput() {
+    MissionUtils.Console.readLine("보너스 번호를 입력하세요 : ", (userInput) => {
+      const userLottoBonus = parseInt(userInput);
+
+      if (userLottoBonus <= 0 || userLottoBonus > 45) {
+        throw new Error("[ERROR] 1 ~ 45 사이의 숫자를 입력하세요");
+      } else if (this.userLottoNumbers.find(el => el === userLottoBonus)) {
+        throw new Error("[ERROR] 기존 입력 번호와 다른 보너스 번호를 입력하세요");
+      }
+
+      this.bonus = userLottoBonus;
+
+      this.checkLotto();
+    });
+  }
+
+  checkLotto() {
+    MissionUtils.Console.print(`${this.counts}개를 구매했습니다.`)
+
+    new Lotto(this.userLottoNumbers);
+  }
+
+
+  play() {
+    this.getUserInput();
+  }
 }
+
+const app = new App();
+app.play();
+
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,72 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.counts = 0;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.makeLottoNumbers(numbers.length, numbers);
   }
 
-  // TODO: 추가 기능 구현
+  makeLottoNumbers(counts, numbers) {
+    const lottoNumbers = [];
+    this.counts = counts;
+    for (let i = 0; i < counts; i++) {
+      const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lotto.sort((a, b) => a - b);
+      lottoNumbers.push(lotto);
+    }
+
+    this.checkLotto(lottoNumbers, numbers);
+  }
+
+  checkLotto(lottoNumbers, numbers) {
+    const resultArr = [];
+    for (let i = 0; i < lottoNumbers.length; i++) {
+      let interSection = numbers.filter(x => lottoNumbers[i].includes(x));
+      resultArr.push(interSection.length);
+    }
+
+    const isBonus = resultArr.findIndex(el => el === 5);
+    this.showResult(resultArr, numbers);
+  }
+
+  showResult(resultArr, numbers) {
+    const showArr = [];
+    for (let i = 3; i <= 6; i++) {
+      showArr.push(resultArr.filter(x => x === i).length);
+    }
+
+    showArr.map((el, index) => {
+      if (index === 0) {
+        MissionUtils.Console.print(`${index + 3}개 일치 (5,000)원 - ${el}개`);
+      }
+
+      if (index === 1) {
+        MissionUtils.Console.print(`${index + 3}개 일치 (50,000)원 - ${el}개`);
+      }
+
+      if (index === 2) {
+        MissionUtils.Console.print(`${index + 3}개 일치 (1,500,000)원 - ${el}개`);
+      }
+
+      if (index === 3) {
+        MissionUtils.Console.print(`${index + 3}개 일치 (2,000,000,000)원 - ${el}개`);
+      }
+
+    })
+
+  }
+
+
+
 }
 
 module.exports = Lotto;
